439f4f554156dede3ffb5b0aa18e554dbd6d529b
Prista86 <101596856+Prista86@users.noreply.github.com>
2022-11-01 16:46:02 +0100

    finito2

--- src/app/finti-eroi.ts
+++ src/app/finti-eroi.ts
@@ -4,6 +4,5 @@ export const fintiEroi : Eroe[] = [
     {id: 2, nome: 'Superman'},
     {id: 3, nome: 'Iron Man'},
     {id: 4, nome: 'Hulk'},
-    {id: 5, nome: 'Captain Marvel'},
-    {id: 6, nome: 'Io'}
+    {id: 5, nome: 'Captain Marvel'}
 ];
